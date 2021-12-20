79c7932db9081242e19975e26deea14ea6a7ab2a
Rostyslav <zachepa.rostislav@gmail.com>
2021-12-21 00:04:29 +0200

    not finish

--- server.js
+++ server.js
@@ -6,23 +6,26 @@ const mongoose = require('mongoose');
 const router = require('./router/index');
 const errorMiddleware = require('./middlewares/error-middleware');
 
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-app.use('/api',router);
+app.use(cors({
+    credentials:true,
+    origin: process.env.CLIENT_URL,
+}));
+app.use('/api', router);
 app.use(errorMiddleware);
 
-const start = async() => {
+const start = async () => {
     try {
-        await mongoose.connect(process.env.DB_URL,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         })
-        app.listen(PORT,()=> console.log(`Server started on PORT ${PORT}`))
-    } catch(e){
+        app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
+    } catch (e) {
         console.log(e)
     }
 }
